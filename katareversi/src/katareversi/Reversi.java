d97b37253cb32ca21a2c3bad4ed2ed5674a31deb
noxaw <maxence.mermoz@icloud.com>
2013-11-13 10:18:30 +0100

    compare par ligne

--- katareversi/src/katareversi/Reversi.java
+++ katareversi/src/katareversi/Reversi.java
@@ -1,23 +1,43 @@
 package katareversi;
 
 public class Reversi {
-	private String plateau = ""+
-			"........\n"+
-			"........\n"+
-			"........\n"+
-			"...BW...\n"+
-			"...WB...\n"+
-			"........\n"+
-			"........\n"+
-			"........\n"+
-			"B";
+	
+	private String plateau;
 
-	public String donnePlateau() {
-		return plateau;
-	}
-
-	public Object donnePossibilites(String input) {
+	public String donnePossibilites(String valeurPlateau) {
+		plateau = valeurPlateau;
+		/*if(joueurEstBlanc()){
+			return 	""+
+					"........\n"+
+					"........\n"+
+					"...0....\n"+
+					"..0BW...\n"+
+					"...WB0..\n"+
+					"....0...\n"+
+					"........\n"+
+					"........\n"+
+					"W";
+		}
 		
+		return 	""+
+				"........\n"+
+				"........\n"+
+				"....0...\n"+
+				"...BW0..\n"+
+				"..0WB...\n"+
+				"...0....\n"+
+				"........\n"+
+				"........\n"+
+				"B";
+		*/
+		if(plateau.contains("BW.")){
+			return plateau.replaceAll("BW.", "BW0");
+		}
+		return valeurPlateau;
+	}
+	
+	public boolean joueurEstBlanc(){
+		return plateau.charAt(plateau.length()-1) == "W".charAt(0);
 	}
 	
 }
--- katareversi/src/katareversi/TestsApp.java
+++ katareversi/src/katareversi/TestsApp.java
@@ -3,10 +3,16 @@ package katareversi;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class TestsApp {
 	
 	private Reversi jeu;
@@ -70,11 +76,77 @@ public class TestsApp {
 		testArrayOutput.add(resultat2);
 	}
 	
-	@Test
-	public void TestPlateauInitialAuxNoirs(){
-		
-		assertEquals(testArrayOutput, jeu.donnePossibilites(testArrayInput));
+	@Parameters
+    public static List<String[]> getParametres() {
+        return Arrays.asList(new String[][] {
+                { 	/*""+
+        				"........\n"+
+        				"........\n"+
+        				"........\n"+
+        				"...BW...\n"+
+        				"...WB...\n"+
+        				"........\n"+
+        				"........\n"+
+        				"........\n"+
+        				"W", 
+        				
+	        					""+
+	        						"........\n"+
+	        						"........\n"+
+	        						"...0....\n"+
+	        						"..0BW...\n"+
+	        						"...WB0..\n"+
+	        						"....0...\n"+
+	        						"........\n"+
+	        						"........\n"+
+	        						"W" },
+                { 	""+
+        				"........\n"+
+        				"........\n"+
+        				"........\n"+
+        				"...BW...\n"+
+        				"...WB...\n"+
+        				"........\n"+
+        				"........\n"+
+        				"........\n"+
+        				"B", 
+        						""+
+	        						"........\n"+
+	        						"........\n"+
+	        						"....0...\n"+
+	        						"...BW0..\n"+
+	        						"..0WB...\n"+
+	        						"...0....\n"+
+	        						"........\n"+
+	        						"........\n"+
+	        						"B"}*/
+                	//ça
+                	"........\n",
+                		//retourne ça
+                		"........\n"},
+                
+                	{
+            		//ça
+                	"...BW...\n",
+                		//retourne ça
+                		"...BW0..\n"}
+                		
+	        						});
+        }
 
-	}
+        
+    private String resultatPossibilites;
+    private String plateauInput;
+
+    public TestsApp(String variable, String resultat) {
+    	resultatPossibilites = resultat;
+    	plateauInput = variable;
+    }
+
+	@Test
+	    public void TestsPossibilites() {
+	        String retour = jeu.donnePossibilites(plateauInput);
+	        assertEquals(resultatPossibilites, retour);
+	    }
 	
 }
