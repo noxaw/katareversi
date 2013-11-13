6288b9dab8cd8c40216a1ff1fc4f4d9753e6663f
noxaw <maxence.mermoz@icloud.com>
2013-11-13 10:31:34 +0100

    Ajout joueur actif et joueur passif

--- katareversi/src/katareversi/Reversi.java
+++ katareversi/src/katareversi/Reversi.java
@@ -3,41 +3,29 @@ package katareversi;
 public class Reversi {
 	
 	private String plateau;
+	private String joueurActif;
+	private String joueurPassif;
 
 	public String donnePossibilites(String valeurPlateau) {
 		plateau = valeurPlateau;
-		/*if(joueurEstBlanc()){
-			return 	""+
-					"........\n"+
-					"........\n"+
-					"...0....\n"+
-					"..0BW...\n"+
-					"...WB0..\n"+
-					"....0...\n"+
-					"........\n"+
-					"........\n"+
-					"W";
-		}
 		
-		return 	""+
-				"........\n"+
-				"........\n"+
-				"....0...\n"+
-				"...BW0..\n"+
-				"..0WB...\n"+
-				"...0....\n"+
-				"........\n"+
-				"........\n"+
-				"B";
-		*/
-		if(plateau.contains("BW.")){
-			return plateau.replaceAll("BW.", "BW0");
+		this.joueurEstBlanc();
+		
+		if(plateau.contains(joueurActif+joueurPassif+".")){
+			return plateau.replaceAll(joueurActif+joueurPassif+".", joueurActif+joueurPassif+"0");
 		}
-		return valeurPlateau;
+		return plateau;
 	}
 	
-	public boolean joueurEstBlanc(){
-		return plateau.charAt(plateau.length()-1) == "W".charAt(0);
+	public void joueurEstBlanc(){
+		if(plateau.charAt(plateau.length()-1) == "W".charAt(0)){
+			joueurActif = "W";
+			joueurPassif = "B";
+		}
+		else{
+			joueurActif = "B";
+			joueurPassif = "W";
+		}
 	}
 	
 }
